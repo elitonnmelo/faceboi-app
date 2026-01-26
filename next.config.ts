3fc6e489f9a790e9b18078337b4d9653e2df4371
elitonnmelo <elitonp.melo@gmail.com>
2026-01-26 11:46:40 -0300

    Build simplificado para teste de deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Vamos deixar o PWA de fora só por um commit para ver se o site sobe
 }
 
 module.exports = nextConfig
